c3dc7a2a8af5061410f0161f1e0580a46a544b28
Creeper96 <a.corda96@gmail.com>
2014-04-21 01:53:29 +0200

    second release test

--- IglesiasHub/src/main/java/me/Creeper96/IglesiasHub/Events.java
+++ IglesiasHub/src/main/java/me/Creeper96/IglesiasHub/Events.java
@@ -1,38 +1,36 @@
-/*    */ package me.Creeper96.IglesiasHub;
-/*    */ 
-/*    */ import org.bukkit.ChatColor;
+package me.Creeper96.IglesiasHub;
+
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-/*    */ import org.bukkit.event.EventHandler;
-/*    */ import org.bukkit.event.Listener;
-/*    */ import org.bukkit.event.block.Action;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerDropItemEvent;
-/*    */ import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-/*    */ import org.bukkit.inventory.ItemStack;
-/*    */ import org.bukkit.inventory.meta.ItemMeta;
-/*    */ 
-/*    */ public class Events
-/*    */   implements Listener
-/*    */ {
-/*    */   @EventHandler
-/*    */   public void onClickItem(PlayerInteractEvent event)
-/*    */   {
-/* 14 */     if ((event.getAction() != Action.RIGHT_CLICK_AIR) && (event.getAction() != Action.LEFT_CLICK_AIR) && (event.getAction() != Action.LEFT_CLICK_BLOCK) && (event.getAction() != Action.LEFT_CLICK_AIR))
-/*    */       return;
-			//System.out.println("Evento Click");
-/* 16 */     for (IglesiasHubItem i : Main.items)
-/*    */     {
-				//System.out.println(event.getPlayer().getItemInHand().getType()+ "-"+ i.id.toUpperCase());
-/* 18 */       if (event.getPlayer().getItemInHand().equals(i.item))
-/*    */         {
-/* 20 */       		i.click(event.getPlayer());
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Events
+  implements Listener
+ {
+  @EventHandler
+  public void onClickItem(PlayerInteractEvent event)
+   {
+   if ((event.getAction() != Action.RIGHT_CLICK_AIR) && (event.getAction() != Action.LEFT_CLICK_AIR) && (event.getAction() != Action.LEFT_CLICK_BLOCK) && (event.getAction() != Action.LEFT_CLICK_AIR))
+      return;
+    for (IglesiasHubItem i : Main.items)
+     {
+      if (event.getPlayer().getItemInHand().equals(i.item))
+         {
+       		i.click(event.getPlayer());
 					event.setCancelled(true);
 					return;
 				}
-/*    */     }
-/*    */   }
+     }
+   }
 
 		@EventHandler
 		public void onJoin(PlayerJoinEvent event)
@@ -83,4 +81,4 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 			}
 		}
 
-/*    */ }
+}
--- IglesiasHub/src/main/java/me/Creeper96/IglesiasHub/Main.java
+++ IglesiasHub/src/main/java/me/Creeper96/IglesiasHub/Main.java
@@ -1,37 +1,37 @@
-/*    */ package me.Creeper96.IglesiasHub;
-/*    */ 
-/*    */ import java.util.ArrayList;
+package me.Creeper96.IglesiasHub;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Server;
-/*    */ import org.bukkit.configuration.file.FileConfiguration;
-/*    */ import org.bukkit.plugin.Plugin;
-/*    */ import org.bukkit.plugin.PluginManager;
-/*    */ import org.bukkit.plugin.java.JavaPlugin;
-/*    */ 
-/*    */ public class Main extends JavaPlugin
-/*    */ {
-/*    */   public static Plugin plugin;
-/*    */   public static Server server;
-/* 15 */   public static List<IglesiasHubItem> items = new ArrayList();
-/*    */ 
-/*    */   public void onEnable() {
-/* 18 */     plugin = this;
-/* 19 */     server = getServer();
-/* 20 */     plugin.saveDefaultConfig();
-/* 21 */     for (int c = 1; c <= 9; ++c)
-/*    */     {
-/* 23 */       if(plugin.getConfig().isSet(""+c))
-/* 24 */        {
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+  public static Plugin plugin;
+  public static Server server;
+  public static List<IglesiasHubItem> items = new ArrayList();
+
+  public void onEnable() {
+     plugin = this;
+    server = getServer();
+    plugin.saveDefaultConfig();
+    for (int c = 1; c <= 9; ++c)
+    {
+      if(plugin.getConfig().isSet(""+c))
+      {
 					items.add(new IglesiasHubItem(c));
 				}	
 			}
-/*    */     
-/* 26 */     server.getPluginManager().registerEvents(new Events(), this);
-/*    */   }
-/*    */ 
-/*    */   public void onDisable() {
-/* 30 */     plugin = this;
-/* 31 */     server = getServer();
-/*    */   }
-/*    */ }
+    
+     server.getPluginManager().registerEvents(new Events(), this);
+   }
+
+   public void onDisable() {
+    plugin = this;
+    server = getServer();
+  }
+ }
